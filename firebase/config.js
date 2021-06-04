0fcaabcaeb8b79a8595c49e6d714828b1ab33ed7
Humberto2911 <linkzen02@gmail.com>
2021-06-04 03:41:39 -0500

    first commit

---
+++ firebase/config.js
@@ -0,0 +1,12 @@
+const firebaseConfig = {
+    
+    apiKey: "AIzaSyD5DjtbBBH_4PzODGRTul1zLRlWwCscf6o",
+    authDomain: "restaurant-7b848.firebaseapp.com",
+    projectId: "restaurant-7b848",
+    storageBucket: "restaurant-7b848.appspot.com",
+    messagingSenderId: "365371618608",
+    appId: "1:365371618608:web:3275273f723b3703aa04f5",
+    measurementId: "G-7QH60GE463"
+  };
+
+export default firebaseConfig;
